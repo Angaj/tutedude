9a76b2b87b9f2cebe6f10093f88f5f09c46a68af
Angaj <angajbrahmankar@gmail.com>
2025-05-20 15:32:24 +0530

    chnaged folder strurcture

---
+++ JS/Project/reaction_timer.js
@@ -0,0 +1,39 @@
+var start;
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function moveShape() {
+  var shape = document.getElementById("shape");
+  var top = Math.random() * 300;
+  var left = Math.random() * 300;
+  var wh = (Math.random() * 200) + 100;
+
+  shape.style.top = top + "px";
+  shape.style.left = left + "px";
+  shape.style.width = wh + "px";
+  shape.style.height = wh + "px";
+  shape.style.backgroundColor = getRandomColor();
+  shape.style.display = "block";
+
+  start = new Date().getTime();
+}
+
+function appearAfterDelay() {
+  setTimeout(moveShape, Math.random() * 2000);
+}
+
+document.getElementById("shape").onclick = function () {
+  var end = new Date().getTime();
+  var timeTaken = (end - start) / 1000;
+  alert("Your reaction time: " + timeTaken + " seconds");
+  this.style.display = "none";
+  appearAfterDelay();
+};
+
+appearAfterDelay();
